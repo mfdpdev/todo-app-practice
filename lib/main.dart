ff0d13c2e794f366efb43a3d1913a59938c74ed8
lord <danipramoedya2002@gmail.com>
2025-07-20 13:57:29 +0700

    feat: implement navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: const Text("Hello, World!")
+          child: const Text("Hellos, World!")
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 1,
+          onTap: (index) {
+            // setState(() {
+            //   _selectedIndex = index;
+            // });
+          },
+          // backgroundColor: Colors.blue,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(top: 16.0), // Padding pada icon
+                child: Icon(Icons.check_box),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(top: 16.0), // Padding pada icon
+                child: Icon(Icons.add),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(top: 16.0), // Padding pada icon
+                child: Icon(Icons.settings),
+              ),
+              label: '',
+            ),
+          ]
         )
       )
     );
   }
 }
+
+// class Header extends StatelessWidget {
+//   const Header({super.key});
+//
+//   @override
+//   Widget build(BuildContext context){
+//     return 
+//   }
+// }

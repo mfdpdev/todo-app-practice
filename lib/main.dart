d069b36ef5955dc5c0889b4784fe3afa46cbb387
lord <danipramoedya2002@gmail.com>
2025-07-21 20:24:20 +0700

    feat(header): Implement header widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,112 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: const Text("Hellos, World!")
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 0.0),
+            child: Container(
+              // color: Colors.grey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 26.0,
+                      right: 26.0,
+                      top: 18.0,
+                      bottom: 18.0,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 8.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: const Text('Sunday',
+                              style: TextStyle(
+                                fontSize: 20,
+                              )
+                            )
+                          ),
+                        ),
+                        Container(
+                          height: 140,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    const Text('22.30', 
+                                      style: TextStyle(
+                                        fontSize: 42,
+                                        fontWeight: FontWeight.bold,
+                                      )
+                                    ),
+                                    const Text('DEC',
+                                      style: TextStyle(
+                                        fontSize: 42,
+                                        fontWeight: FontWeight.bold,
+                                      )
+                                    )
+                                  ]
+                                )
+                              ),
+                              Container(
+                                width: 1,               // lebar garis
+                                height: double.infinity,             // tinggi garis
+                                color: Colors.grey,     // warna garis
+                                margin: EdgeInsets.symmetric(horizontal: 8),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        const Text('1:20 PM', 
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                          )
+                                        ),
+                                        const Text('New York'),
+                                      ]
+                                    ),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        const Text('6:20 PM', 
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                          )
+                                        ),
+                                        const Text('UK'),
+                                      ]
+                                    ),
+                                  ]
+                                )
+                              ),
+                            ]
+                          ),
+                        )
+                      ]
+                    )
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                      child: Center(
+                        child: const Text('Hello, World!')
+                      )
+                    )
+                  )
+                ]
+              )
+            )
+          )
         ),
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: 1,
